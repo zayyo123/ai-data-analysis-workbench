72ab7124527f30c29a37f8523d4701cbe0d340fe
zayyo123 <764439554@qq.com>
2026-05-23 04:11:17 +0800

    ci: run local and full-stack e2e

--- playwright.config.ts
+++ playwright.config.ts
@@ -16,7 +16,10 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+      use: {
+        ...devices['Desktop Chrome'],
+        ...(process.env.CI ? {} : { channel: 'chrome' }),
+      },
     },
   ],
 })
